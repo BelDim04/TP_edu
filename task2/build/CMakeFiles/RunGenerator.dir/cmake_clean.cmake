f78c0feaba3561d47f1e2f43b0a5874aaf790dfa
BelDim04 <beldim04@gmail.com>
2022-03-11 16:32:33 +0300

    task2

--- task2/build/CMakeFiles/RunGenerator.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../A/index.h"
-  "CMakeFiles/RunGenerator"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/RunGenerator.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- task2/build/CMakeFiles/RunGenerator.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for RunGenerator.
-# This may be replaced when dependencies are built.
